da872c2f858226421d7afa4b37b34d934128fc87
fummy00-g <mink22001@yahoo.co.jp>
2023-10-01 00:02:11 +0900

    割り算を追加

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ def mul(a, b):
     return a * b
 
 
+def dev(a, b):
+    return a / b
+
+
 def main():
     print(add(1, 2))
     print(sub(2, 1))
     print(mul(1, 2))
+    print(dev(2, 1))
 
 
 if __name__ == '__main__':
